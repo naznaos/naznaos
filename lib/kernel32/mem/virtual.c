e5343d33b067f6e7b5ebf61dc06502d2c09cd33b
Nipfswd <allaj0412@gmail.com>
2026-03-24 14:10:55 +0100

    Initial revision

---
+++ lib/kernel32/mem/virtual.c
@@ -0,0 +1,18 @@
+/*
+ * COPYRIGHT:            See COPYING in the top level directory
+ * PROJECT:              NaznaOS kernel
+ * FILE:                 lib/kernel32/mem/virtual.c
+ * PURPOSE:              Passing the Virtualxxx functions onto the kernel
+ * PROGRAMMER:           Noah Juopperi (welch@mcmail.com)
+ */
+
+/* INCLUDES ******************************************************************/
+
+#include <windows.h>
+
+/* FUNCTIONS *****************************************************************/
+
+WINBOOL STDCALL VirtualFree(LPVOID lpAddress, DWORD dwSize, DWORD dwFreeType)
+{
+   return(VirtualFreeEx(GetCurrentProcess(),lpAddress,dwSize,dwFreeType));
+}
